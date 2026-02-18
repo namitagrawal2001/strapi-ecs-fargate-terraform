f25fe7d4bad22dfba9a00641a9638f69645d92dc
Namit Agrawal <namitagrawal58@gmail.com>
2026-02-18 18:35:04 +0530

    Scale ECS service to zero to avoid cost

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -81,7 +81,7 @@ resource "aws_ecs_service" "service" {
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   launch_type     = "FARGATE"
-  desired_count   = 1
+  desired_count   = 0
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
